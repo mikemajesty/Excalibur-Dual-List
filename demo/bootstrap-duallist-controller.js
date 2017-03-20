a1ce6adc30846753b5a7fdfc3820ee6f79c4a7e5
mikemajesty <mikemajesty2011@gmail.com>
2017-03-19 23:21:07 -0300

    add all dependencies

---
+++ demo/bootstrap-duallist-controller.js
@@ -0,0 +1,34 @@
+(angular);
+(function(angular) {
+'use strict';
+angular.module('app')
+  .controller('BoostrapDualListController', ['$scope', ($scope) => {
+
+      $scope.options = {
+        title: 'Demo: your title',
+        helpMessage: 'Demo: your help message',
+        orderProperty: '[name, category]',
+        items: [
+          { 'category': 'FF9', 'name': 'Zidane' },
+          { 'category': 'FF9', 'name': 'Garnet' },
+          { 'category': 'FF9', 'name': 'Vivi' },
+
+          { 'category': 'FF8', 'name': 'Squall Leonhart' },
+          { 'category': 'FF8', 'name': 'Quistis Trepe' },
+          { 'category': 'FF8', 'name': 'Zell Dincht' },
+
+          { 'category': 'FF7', 'name': 'Cloud Strife' },
+          { 'category': 'FF7', 'name': 'Aerith Gainsborough' },
+          { 'category': 'FF7', 'name': 'Vincent Valentine' },
+
+        ],
+        categories: [
+          { 'name': 'FF9' },
+          { 'name': 'FF8' },
+          { 'name': 'FF7' }
+        ],
+        selectedItems: []
+      };
+			
+  }]);
+})(window.angular);
