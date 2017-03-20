d5d9139d5522c5e8e21986198837b196eb55cac8
mikemajesty <mikemajesty2011@gmail.com>
2017-03-20 00:18:24 -0300

    refactor lib

--- demo/bootstrap-duallist-controller.js
+++ demo/bootstrap-duallist-controller.js
@@ -1,33 +1,44 @@
 (function(angular) {
-'use strict';
-angular.module('app')
-  .controller('BoostrapDualListController', ['$scope', function ($scope) {
+  'use strict';
+  angular.module('app')
+    .controller('BoostrapDualListController', ['$scope', '$filter', function($scope, $filter) {
+
+      var items = [
+        { 'category': 'FF9', 'name': 'Zidane' },
+        { 'category': 'FF9', 'name': 'Garnet' },
+        { 'category': 'FF9', 'name': 'Vivi' },
+
+        { 'category': 'FF8', 'name': 'Squall Leonhart' },
+        { 'category': 'FF8', 'name': 'Quistis Trepe' },
+        { 'category': 'FF8', 'name': 'Zell Dincht' },
+
+        { 'category': 'FF7', 'name': 'Cloud Strife' },
+        { 'category': 'FF7', 'name': 'Aerith Gainsborough' },
+        { 'category': 'FF7', 'name': 'Vincent Valentine' },
+
+      ];
+
+      var allCategories = items.map( function (item) { return item.category; });
+
+      var uniqueCategories = unique(allCategories);
+
+      console.log('teste', uniqueCategories)
 
       $scope.options = {
         title: 'Demo: your title',
         helpMessage: 'Demo: your help message',
-        orderProperty: '[name, category]',
-        items: [
-          { 'category': 'FF9', 'name': 'Zidane' },
-          { 'category': 'FF9', 'name': 'Garnet' },
-          { 'category': 'FF9', 'name': 'Vivi' },
-
-          { 'category': 'FF8', 'name': 'Squall Leonhart' },
-          { 'category': 'FF8', 'name': 'Quistis Trepe' },
-          { 'category': 'FF8', 'name': 'Zell Dincht' },
-
-          { 'category': 'FF7', 'name': 'Cloud Strife' },
-          { 'category': 'FF7', 'name': 'Aerith Gainsborough' },
-          { 'category': 'FF7', 'name': 'Vincent Valentine' },
-
-        ],
-        categories: [
-          { 'name': 'FF9' },
-          { 'name': 'FF8' },
-          { 'name': 'FF7' }
-        ],
+        orderProperty: '[category, name]',
+        items: items,
+        categories: uniqueCategories,
         selectedItems: []
       };
-			
-  }]);
+
+    }]);
+
+  function unique(list) {
+    return list.filter(function(item, pos) {
+      return list.indexOf(item) == pos;
+    });
+  }
+
 })(window.angular);
