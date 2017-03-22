2b97e4bada20c6d00a70bb9d9e52b758690c54ce
mikemajesty <mikemajesty2011@gmail.com>
2017-03-21 22:21:21 -0300

    add full js lib

---
+++ dist/js/excalibur-duallist.js
@@ -0,0 +1,25 @@
+angular.module("app", []).directive("excaliburDualList", [function() {
+
+	return {
+		restrict: 'E',
+		scope: {
+			options: '='
+		},
+		controller: function($scope) {
+
+			$scope.transfer = function(from, to, index) {
+				if (index >= 0) {
+					to.push(from[index]);
+					from.splice(index, 1);
+				} else {
+					for (var i = 0; i < from.length; i++) {
+						to.push(from[i]);
+					}
+					from.length = 0;
+				}
+			};
+		},
+		templateUrl: 'bower_components/excalibur-duallist/excalibur-duallist.html'
+	};
+
+}]);
