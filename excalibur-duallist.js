0ad8140bf1f4a5e2471f54565bf0060fce4234ec
mikemajesty <mikemajesty2011@gmail.com>
2017-03-24 12:47:54 -0300

    fix controller bug

--- excalibur-duallist.js
+++ excalibur-duallist.js
@@ -19,7 +19,7 @@ angular.module("excaliburDualList", []).directive("excaliburDualList", [function
 				}
 			};
 		},
-		templateUrl: 'bower_components/excalibur-duallist/excalibur-duallist.html'
+		templateUrl: 'excalibur-duallist.html'
 	};
 
 }]);
