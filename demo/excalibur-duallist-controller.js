d4c9032450a5e7e42584d009677749f1c466e8b2
mikemajesty <mikemajesty2011@gmail.com>
2017-04-02 00:05:39 -0300

    change variable name

--- demo/excalibur-duallist-controller.js
+++ demo/excalibur-duallist-controller.js
@@ -8,7 +8,7 @@
       $scope.options = {
         title: {label: 'Demo: your title', helpMessage: 'Demo: your help message'},
         selectOptions: {initialText: "ALL", isDisable: false, uppercase: true},
-        selectHeight: 'md',
+        boxItemsHeight: 'md',
         items: [
           { 'category': 'FF8', 'name': 'Squall Leonhart' },
           { 'category': 'FF8', 'name': 'Quistis Trepe' },
